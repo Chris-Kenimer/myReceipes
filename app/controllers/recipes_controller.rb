c72005abf15956b847c8ad192d3f57052185fab1
chris_kenimer <ckenimer@hotmail.com>
2015-11-10 03:06:27 +0000

    Pagination

--- app/controllers/recipes_controller.rb
+++ app/controllers/recipes_controller.rb
@@ -1,6 +1,7 @@
 class RecipesController < ApplicationController
     def index
-        @recipes = Recipe.all
+        #@recipes = Recipe.all.sort_by{|likes| likes.thumbs_up_total }.reverse
+        @recipes = Recipe.paginate(page: params[:page], per_page: 4)
     end
     def show 
         @recipe = Recipe.find(params[:id])
@@ -30,6 +31,17 @@ class RecipesController < ApplicationController
             redirect_to recipe_path(@recipe)
         else
             render :edit
+        end
+    end
+    def like
+        @recipe = Recipe.find(params[:id])
+        like = Like.create(like: params[:like], chef: Chef.first, recipe: @recipe)
+        if like.valid?
+        flash[:success] = "Your selection was successfully"
+        redirect_to :back
+        else 
+            flash[:danger] = " You can only like/dislike a recipe once"
+            redirect_to :back
         end
     end
     private 
